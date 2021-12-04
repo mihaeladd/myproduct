313ecbfb095c2bee9f5a06fb700e0b9f2a63ae3b
mihaeladd <mihaeladodon10@gmail.com>
2021-12-04 13:51:44 +0200

    added security configuration for authentication

---
+++ src/main/java/com/myproduct/myproduct/configuration/UserDetailsImpl.java
@@ -0,0 +1,54 @@
+package com.myproduct.myproduct.configuration;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+public class UserDetailsImpl implements UserDetails {
+
+    private String userName;
+
+    public UserDetailsImpl(String userName){
+        this.userName = userName;
+    }
+    public UserDetailsImpl(){
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Arrays.asList(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    @Override
+    public String getPassword() {
+        return "pass";
+    }
+
+    @Override
+    public String getUsername() {
+        return userName;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
